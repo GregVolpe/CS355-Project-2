8b43d6e15b9f7d5abfaaf1c44f676d6624dc053d
Greg Volpe <volpe1988@gmail.com>
2014-11-30 13:29:35 -0500

    Create empty Problem1

---
+++ Assignment2_CS355/Assignment2_CS355/Problem1.cpp
@@ -0,0 +1,15 @@
+#include "Problem1.h"
+
+
+Problem1::Problem1()
+{
+}
+
+
+Problem1::~Problem1()
+{
+}
+
+void Problem1::runProblem1(){
+
+}
---
+++ Assignment2_CS355/Assignment2_CS355/Problem1.h
@@ -0,0 +1,10 @@
+#pragma once
+class Problem1
+{
+public:
+	Problem1();
+	~Problem1();
+
+	void runProblem1();
+};
+
